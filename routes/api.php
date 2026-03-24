015ba31b9cb9a0c67a542a52c22147c5dd7359e5
nagaprasad-ts <tsnagaprasadts@gmail.com>
2026-03-24 14:47:19 +0530

    udpate in url

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/razorpay/webhook', [PaymentController::class, 'webhook'])->name('razorpay.webhook');
+// Route::post('/razorpay/webhook', [PaymentController::class, 'webhook'])->name('razorpay.webhook');
