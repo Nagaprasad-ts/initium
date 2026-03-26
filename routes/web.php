6080fc4fb10bc4cd90dd64174524b541e81d5dac
root <root@ip-192-168-1-79.ap-south-1.compute.internal>
2026-03-26 08:52:14 +0000

    webhook fix

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/registration/success/{id}', [RegistrationController::class, 'succes
 Route::post('/registration/verify', [RegistrationController::class, 'verify'])->name('registration.verify');
 
 Route::post('/registration/submit', [RegistrationController::class, 'store'])->name('registration.submit');
-Route::post('/razorpay/webhook', [PaymentController::class, 'webhook'])->name('razorpay.webhook');
+// Route::post('/razorpay/webhook', [PaymentController::class, 'webhook'])->name('razorpay.webhook');
 
 Route::get('/test', [EventController::class, 'test'])->name('test');
 
